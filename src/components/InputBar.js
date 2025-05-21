262cf19ce3c3249432a648ff9913c4d0f1dde45c
unknown <julianact2004@gmail.com>
2025-05-21 09:08:11 -0300

    preparação do ambiente

---
+++ src/components/InputBar.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function InputBar() {
+  return (
+    <View>
+      <Text>InputBar</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    
+})
