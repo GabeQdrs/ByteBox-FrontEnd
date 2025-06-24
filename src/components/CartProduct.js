745f5215a1a58ce1ceb2c44ea9521cb5d78efb30
gabriel henrique  techio <gabrielhenriquetechio123@gmail.com>
2025-06-24 15:33:18 -0300

    OrdersScreen:em andamento

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -46,7 +46,7 @@ const CartProduct = ({ product, onPress }) => {
   return (
   <View style={styles.productCard}>
 
-    <Image source={livroImg} style={styles.productImage} />
+    
 
   
     <View style={styles.productDetails}>
